7e6c3f071288ffb9209e5acc1c2d34924d911be3
Martin Westerhof <a.m.westerhof@gmail.com>
2023-07-12 10:18:34 +0200

    initial setup cloud based API

---
+++ info.ts
@@ -0,0 +1,37 @@
+// let sales: number = 123_456_789;
+// let course: string = 'TypeScript';
+// let is_piblished: boolean = true;
+
+// function render(document: any) {
+//     console.log(document);
+// }
+
+//let number: number[] = [1, 2, 3] 
+
+// function calculateTax(income: number, taxYear: number) :number {
+//     if((taxYear || 2022) < 2022) 
+//         return income * 1.2;
+//     return income * 1.3;
+// }
+
+// function calculateTax(income: number, taxYear = 2022) :number {
+//     if((taxYear || 2022) < 2022) 
+//         return income * 1.2;
+//     return income * 1.3;
+// }
+
+// type Employee = {
+//     readonly id: number,
+//     name: string,
+//     retire: (date: Date) => void
+// }
+
+// let employee: Employee = {
+//     id: 1,
+//     name: 'stan',
+//     retire: (date: Date) => {
+//         console.log(date);
+//     }
+// }
+
+// console.log(employee);
