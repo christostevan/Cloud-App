0ae3f6e7ad1aea793d32bc23271ad50f4bf7269e
Martin Westerhof <a.m.westerhof@gmail.com>
2023-07-13 13:44:22 +0200

    Update to functionality
    Deleted test functions,
    Update to Weather call,
    Add function that will get the current time.
    Add function that will check for weather conditions

--- dist/controllers/weatherAlarm.js
+++ dist/controllers/weatherAlarm.js
@@ -12,19 +12,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.currentWeather = void 0;
+exports.processData = exports.currentWeather = void 0;
 const axios_1 = __importDefault(require("axios"));
 const currentWeather = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { longitude, latitude } = req.query;
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=precipitation&timezone=Europe%2FLondon&forecast_days=1`;
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain,snowfall&forecast_days=1`;
         const response = yield axios_1.default.get(url);
         const data = response.data;
-        res.json(data);
+        const processedData = yield processData(data);
+        res.json(processedData);
     }
     catch (error) {
         res.status(500).json({ error: 'An error occurred while fetching the weather data.' });
     }
 });
 exports.currentWeather = currentWeather;
+function processData(data) {
+    const currentHour = new Date().getUTCHours();
+    const hourlyData = data.hourly.time;
+    let timeID = null;
+    for (let i = 0; i < hourlyData.length; i++) {
+        const date = new Date(hourlyData[i]);
+        const hour = date.getUTCHours();
+        if (hour === currentHour) {
+            timeID = i;
+            console.log(timeID);
+            console.log(hourlyData[timeID]);
+            break;
+        }
+    }
+    console.log(currentHour);
+    console.log(hourlyData);
+    return data;
+}
+exports.processData = processData;
+;
 //# sourceMappingURL=weatherAlarm.js.map
