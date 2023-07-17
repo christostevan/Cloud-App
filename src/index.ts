e3e6e6d680409fbcb3084d031bd71d84156d8c15
christo_stevan <christostevan2501@gmail.com>
2023-07-17 12:18:42 +0200

    Installed Cors
    Enable 'Access-Control-Allow-Origin' to read from localhost API

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
-
 import { currentWeather } from './controllers/weatherAlarm';
 /**
  * Application startup 
  */
+const cors = require('cors');
+
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+
+  
 const app = express();
+app.use(cors(corsOptions));
 
 const port = 3000;
 
